d6cfeb47191cf3e2f58f15f2280e05c305c90f4b
Dessalines <tyhou13@gmx.com>
2020-10-03 11:03:29 -0500

    Adding librejs and a banner to javascript. Fixes #50

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,14 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const nodeExternals = require('webpack-node-externals');
 const CopyPlugin = require('copy-webpack-plugin');
 const path = require('path');
+const webpack = require('webpack');
+
+const banner = `
+  hash:[hash], chunkhash:[chunkhash], name:[name], filebase:[filebase], query:[query], file:[file]
+  Source code: https://github.com/LemmyNet/lemmy-ui
+  Created by dessalines
+  @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL v3.0
+  `;
 
 module.exports = function (env, _) {
   const base = {
@@ -42,6 +50,9 @@ module.exports = function (env, _) {
       }),
       new CopyPlugin({
         patterns: [{ from: './src/assets', to: './assets' }],
+      }),
+      new webpack.BannerPlugin({
+        banner,
       }),
     ],
   };
