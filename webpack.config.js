d64507f64de67f5ea73dc683cb2ce9e2327002bd
Orvar Segerström <orvarsegerstrom@gmail.com>
2020-10-25 20:46:08 +0100

    Combine webpack configs to build everything at once

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
+const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const nodeExternals = require('webpack-node-externals');
 const CopyPlugin = require('copy-webpack-plugin');
-const path = require('path');
-const webpack = require('webpack');
+const { merge } = require('lodash');
 
 const banner = `
   hash:[contentHash], chunkhash:[chunkhash], name:[name], filebase:[base], query:[query], file:[file]
@@ -11,73 +11,90 @@ const banner = `
   @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL v3.0
   `;
 
-module.exports = function (env, _) {
-  const platform = env.platform || 'server';
+const base = {
+  output: {
+    filename: 'js/server.js',
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+  performance: {
+    hints: false,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(scss|css)$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(js|jsx|tsx|ts)$/, // All ts and tsx files will be process by
+        exclude: /node_modules/, // ignore node_modules
+        loader: 'babel-loader',
+      },
+      // Due to some weird babel issue: https://github.com/webpack/webpack/issues/11467
+      {
+        test: /\.m?js/,
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'styles/styles.css',
+    }),
+    new CopyPlugin({
+      patterns: [{ from: './src/assets', to: './assets' }],
+    }),
+    new webpack.BannerPlugin({
+      banner,
+    }),
+  ],
+};
 
-  const base = {
-    // mode is set by package.json flags
-    entry: './src/server/index.tsx', // Point to main file
+const createServerConfig = (env, mode) => {
+  const config = merge({}, base, {
+    mode,
+    entry: './src/server/index.tsx',
     output: {
       filename: 'js/server.js',
-      publicPath: '/',
     },
-    resolve: {
-      extensions: ['.js', '.jsx', '.ts', '.tsx'],
-    },
-    performance: {
-      hints: false,
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(scss|css)$/i,
-          use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-        },
-        {
-          test: /\.(js|jsx|tsx|ts)$/, // All ts and tsx files will be process by
-          exclude: /node_modules/, // ignore node_modules
-          loader: 'babel-loader',
-        },
-        // Due to some weird babel issue: https://github.com/webpack/webpack/issues/11467
-        {
-          test: /\.m?js/,
-          resolve: {
-            fullySpecified: false,
-          },
-        },
-      ],
-    },
-    devServer: {
-      host: '0.0.0.0',
-      contentBase: 'src/',
-      historyApiFallback: true,
-    },
-    plugins: [
-      new MiniCssExtractPlugin({
-        filename: 'styles/styles.css',
-      }),
-      new CopyPlugin({
-        patterns: [{ from: './src/assets', to: './assets' }],
-      }),
-      new webpack.BannerPlugin({
-        banner,
-      }),
-    ],
-    cache: {
-      type: 'filesystem',
-      name: platform,
-    },
-  };
+    target: 'node',
+    externals: [nodeExternals(), 'inferno-helmet'],
+  });
 
-  // server-specific configuration
-  if (platform === 'server') {
-    base.target = 'node';
-    base.externals = [nodeExternals(), 'inferno-helmet'];
+  if (mode === 'development') {
+    config.cache = {
+      type: 'filesystem',
+      name: 'server',
+    };
   }
-  // client-specific configurations
-  if (platform === 'client') {
-    base.entry = './src/client/index.tsx';
-    base.output.filename = 'js/client.js';
-  }
-  return base;
+
+  return config;
 };
+const createClientConfig = (env, mode) => {
+  const config = merge({}, base, {
+    mode,
+    entry: './src/client/index.tsx',
+    output: {
+      filename: 'js/client.js',
+    },
+  });
+
+  if (mode === 'development') {
+    config.cache = {
+      type: 'filesystem',
+      name: 'client',
+    };
+  }
+
+  return config;
+};
+
+module.exports = (env, properties) => [
+  createServerConfig(env, properties.mode || 'development'),
+  createClientConfig(env, properties.mode || 'development'),
+];
