fa971d6755aac4420900954b8fc0ddfcf1153dd1
Dessalines <tyhou13@gmx.com>
2020-11-25 14:28:45 -0600

    Fixing update translation script.

--- update_translations.sh
+++ update_translations.sh
@@ -2,7 +2,9 @@
 pushd ../lemmy-translations
 git fetch weblate
 git merge weblate/main
+git push
 popd
 git submodule update --remote
 git add lemmy-translations
 git commit -m"Updating translations."
+git push
