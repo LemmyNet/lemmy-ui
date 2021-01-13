2d9baaea96f793d5fe6dfaf3cbd57a7f65d3b42f
Dessalines <tyhou13@gmx.com>
2021-01-13 09:39:10 -0500

    fixing drone.

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,9 @@
 
 new_tag="$1"
 
-sudo docker build . --tag dessalines/lemmy-ui:$new_tag
-sudo docker push dessalines/lemmy-ui:$new_tag
+# Old deploy
+# sudo docker build . --tag dessalines/lemmy-ui:$new_tag
+# sudo docker push dessalines/lemmy-ui:$new_tag
 
 git tag $new_tag
 git push origin $new_tag
