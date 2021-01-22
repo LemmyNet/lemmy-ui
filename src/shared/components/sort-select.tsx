f008af8497f74a10ac58416c74ea7fc44eea66f0
Dessalines <tyhou13@gmx.com>
2021-01-22 12:19:12 -0500

    Fixing top_time translations.

--- src/shared/components/sort-select.tsx
+++ src/shared/components/sort-select.tsx
@@ -50,10 +50,10 @@ export class SortSelect extends Component<SortSelectProps, SortSelectState> {
           <option value={SortType.New}>{i18n.t('new')}</option>
           <option disabled>─────</option>
           <option value={SortType.TopDay}>{i18n.t('top_day')}</option>
-          <option value={SortType.TopWeek}>{i18n.t('week')}</option>
-          <option value={SortType.TopMonth}>{i18n.t('month')}</option>
-          <option value={SortType.TopYear}>{i18n.t('year')}</option>
-          <option value={SortType.TopAll}>{i18n.t('all')}</option>
+          <option value={SortType.TopWeek}>{i18n.t('top_week')}</option>
+          <option value={SortType.TopMonth}>{i18n.t('top_month')}</option>
+          <option value={SortType.TopYear}>{i18n.t('top_year')}</option>
+          <option value={SortType.TopAll}>{i18n.t('top_all')}</option>
         </select>
         <a
           className="text-muted"
