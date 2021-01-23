f2999df3004b9c9d7f08e7e9fb0b40cc0102cadc
Dessalines <tyhou13@gmx.com>
2021-01-23 17:23:49 -0500

    Use display titles for community links. Fixes #98

--- src/shared/components/community-link.tsx
+++ src/shared/components/community-link.tsx
@@ -20,20 +20,22 @@ export class CommunityLink extends Component<CommunityLinkProps, any> {
 
   render() {
     let community = this.props.community;
-    let name_: string, link: string;
+    let name_: string, title: string, link: string;
     let local = community.local == null ? true : community.local;
     if (local) {
       name_ = community.name;
+      title = community.title;
       link = `/c/${community.name}`;
     } else {
       name_ = `${community.name}@${hostname(community.actor_id)}`;
+      title = `${community.title}@${hostname(community.actor_id)}`;
       link = !this.props.realLink
         ? `/community/${community.id}`
         : community.actor_id;
     }
 
     let apubName = `!${name_}`;
-    let displayName = this.props.useApubName ? apubName : name_;
+    let displayName = this.props.useApubName ? apubName : title;
     return (
       <Link
         title={apubName}
