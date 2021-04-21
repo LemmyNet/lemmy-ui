5da89c083255cf589dc6bf9520c83671808d1e62
Dessalines <tyhou13@gmx.com>
2021-04-21 10:21:38 -0400

    Add Korean language

--- src/shared/i18next.ts
+++ src/shared/i18next.ts
@@ -33,6 +33,7 @@ import { oc } from "./translations/oc";
 import { hr } from "./translations/hr";
 import { th } from "./translations/th";
 import { bg } from "./translations/bg";
+import { ko } from "./translations/ko";
 
 // https://github.com/nimbusec-oss/inferno-i18next/blob/master/tests/T.test.js#L66
 const resources = {
@@ -69,6 +70,7 @@ const resources = {
   hr,
   th,
   bg,
+  ko,
 };
 
 function format(value: any, format: any): any {
