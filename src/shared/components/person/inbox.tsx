68514a3b4e11e5361eccfb5e5174f9d65685e101
Dessalines <tyhou13@gmx.com>
2021-10-14 15:30:06 -0400

    Simplifying getunreadcount.

--- src/shared/components/person/inbox.tsx
+++ src/shared/components/person/inbox.tsx
@@ -533,9 +533,18 @@ export class Inbox extends Component<any, InboxState> {
     i.state.replies = [];
     i.state.mentions = [];
     i.state.messages = [];
-    i.sendUnreadCount();
+    UserService.Instance.unreadInboxCountSub.next(0);
     window.scrollTo(0, 0);
     i.setState(i.state);
+  }
+
+  sendUnreadCount(read: boolean) {
+    let urcs = UserService.Instance.unreadInboxCountSub;
+    if (read) {
+      urcs.next(urcs.getValue() - 1);
+    } else {
+      urcs.next(urcs.getValue() + 1);
+    }
   }
 
   parseMessage(msg: any) {
@@ -551,7 +560,6 @@ export class Inbox extends Component<any, InboxState> {
       this.state.replies = data.replies;
       this.state.combined = this.buildCombined();
       this.state.loading = false;
-      this.sendUnreadCount();
       window.scrollTo(0, 0);
       this.setState(this.state);
       setupTippy();
@@ -559,7 +567,6 @@ export class Inbox extends Component<any, InboxState> {
       let data = wsJsonToRes<GetPersonMentionsResponse>(msg).data;
       this.state.mentions = data.mentions;
       this.state.combined = this.buildCombined();
-      this.sendUnreadCount();
       window.scrollTo(0, 0);
       this.setState(this.state);
       setupTippy();
@@ -567,7 +574,6 @@ export class Inbox extends Component<any, InboxState> {
       let data = wsJsonToRes<PrivateMessagesResponse>(msg).data;
       this.state.messages = data.private_messages;
       this.state.combined = this.buildCombined();
-      this.sendUnreadCount();
       window.scrollTo(0, 0);
       this.setState(this.state);
       setupTippy();
@@ -635,7 +641,7 @@ export class Inbox extends Component<any, InboxState> {
             data.private_message_view.private_message.read;
         }
       }
-      this.sendUnreadCount();
+      this.sendUnreadCount(data.private_message_view.private_message.read);
       this.setState(this.state);
     } else if (op == UserOperation.MarkAllAsRead) {
       // Moved to be instant
@@ -671,7 +677,8 @@ export class Inbox extends Component<any, InboxState> {
         found.comment.read = combinedView.comment.read =
           data.comment_view.comment.read;
       }
-      this.sendUnreadCount();
+
+      this.sendUnreadCount(data.comment_view.comment.read);
       this.setState(this.state);
       setupTippy();
     } else if (op == UserOperation.MarkPersonMentionAsRead) {
@@ -719,7 +726,7 @@ export class Inbox extends Component<any, InboxState> {
             data.person_mention_view.person_mention.read;
         }
       }
-      this.sendUnreadCount();
+      this.sendUnreadCount(data.person_mention_view.person_mention.read);
       this.setState(this.state);
     } else if (op == UserOperation.CreateComment) {
       let data = wsJsonToRes<CommentResponse>(msg).data;
@@ -764,7 +771,7 @@ export class Inbox extends Component<any, InboxState> {
           }
           this.state.combined = this.buildCombined();
         }
-        this.sendUnreadCount();
+        this.sendUnreadCount(true);
         this.setState(this.state);
         setupTippy();
         // TODO this seems wrong, you should be using form_id
@@ -805,25 +812,6 @@ export class Inbox extends Component<any, InboxState> {
         toast(i18n.t("report_created"));
       }
     }
-  }
-
-  sendUnreadCount() {
-    UserService.Instance.unreadInboxCountSub.next(this.unreadCount());
-  }
-
-  unreadCount(): number {
-    return (
-      this.state.replies.filter(r => !r.comment.read).length +
-      this.state.mentions.filter(r => !r.person_mention.read).length +
-      this.state.messages.filter(
-        r =>
-          UserService.Instance.myUserInfo &&
-          !r.private_message.read &&
-          // TODO also seems very strange and wrong
-          r.creator.id !==
-            UserService.Instance.myUserInfo.local_user_view.person.id
-      ).length
-    );
   }
 
   isMention(view: any): view is PersonMentionView {
