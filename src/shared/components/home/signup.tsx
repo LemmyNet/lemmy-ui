a3f904a87542e56da685c397d70ebd94be4d00da
Kradyz <81911574+Kradyz@users.noreply.github.com>
2021-12-15 03:24:55 +0100

    Update signup.tsx

--- src/shared/components/home/signup.tsx
+++ src/shared/components/home/signup.tsx
@@ -144,7 +144,7 @@ export class Signup extends Component<any, State> {
               onInput={linkEvent(this, this.handleRegisterUsernameChange)}
               required
               minLength={3}
-              pattern="[a-zA-Z0-9_]+"
+              pattern="[a-z0-9_]+"
               title={i18n.t("community_reqs")}
             />
           </div>
