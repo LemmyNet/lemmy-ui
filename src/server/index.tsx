eea2e4c8ebd4ce764274cbb6b4dc163a79eccbd5
Dessalines <tyhou13@gmx.com>
2022-05-24 12:38:35 -0400

    Change name to LEMMY_UI_CUSTOM_HTML_HEADER

--- src/server/index.tsx
+++ src/server/index.tsx
@@ -36,7 +36,7 @@ if (process.env.NODE_ENV !== "development") {
     next();
   });
 }
-const customHtml = process.env["LEMMY_UI_CUSTOM_HTML"] || "";
+const customHtmlHeader = process.env["LEMMY_UI_CUSTOM_HTML_HEADER"] || "";
 
 server.use(express.json());
 server.use(express.urlencoded({ extended: false }));
@@ -201,7 +201,7 @@ server.get("/*", async (req, res) => {
            ${erudaStr}
 
            <!-- Custom injected script -->
-           ${customHtml}
+           ${customHtmlHeader}
 
            ${helmet.title.toString()}
            ${helmet.meta.toString()}
