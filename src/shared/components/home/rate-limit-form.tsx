b1a7a679f011e11bb521efadf9b33a80a0b5b4a7
abias <abias1122@gmail.com>
2023-05-20 15:39:12 -0400

    Refactor tabs into reuseable component

---
+++ src/shared/components/home/rate-limit-form.tsx
@@ -0,0 +1,11 @@
+import { Component } from "inferno";
+
+export default class RateLimitForm extends Component {
+  constructor(props, context) {
+    super(props, context);
+  }
+
+  render() {
+    return <></>;
+  }
+}
