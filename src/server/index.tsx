201a31a677b6b439ed73212fe6a5db44e7c235af
0xDEADCADE <69792955+0xDEADCADE@users.noreply.github.com>
2023-06-08 14:41:42 +0000

    Fix formatting

--- src/server/index.tsx
+++ src/server/index.tsx
@@ -348,7 +348,9 @@ async function createSsrHtml(root: string, isoData: IsoDataOptionalSite) {
   <!DOCTYPE html>
   <html ${helmet.htmlAttributes.toString()} lang="en">
   <head>
-  <script>window.isoData = ${md.utils.escapeHtml(JSON.stringify(isoData))}</script>
+  <script>window.isoData = ${md.utils.escapeHtml(
+    JSON.stringify(isoData)
+  )}</script>
   <script>window.lemmyConfig = ${serialize(config)}</script>
 
   <!-- A remote debugging utility for mobile -->
