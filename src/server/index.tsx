05b859cbf5e67b7b2d7378735d41e58258c43f06
Felix Ableitner <me@nutomic.com>
2023-06-09 13:41:25 +0200

    Use sanitize, then restore > chars for markdown render

--- src/server/index.tsx
+++ src/server/index.tsx
@@ -9,6 +9,7 @@ import IsomorphicCookie from "isomorphic-cookie";
 import { GetSite, GetSiteResponse, LemmyHttp, Site } from "lemmy-js-client";
 import path from "path";
 import process from "process";
+import sanitize from "sanitize-html";
 import serialize from "serialize-javascript";
 import sharp from "sharp";
 import { App } from "../shared/components/app/app";
@@ -347,9 +348,7 @@ async function createSsrHtml(root: string, isoData: IsoDataOptionalSite) {
   <!DOCTYPE html>
   <html ${helmet.htmlAttributes.toString()} lang="en">
   <head>
-  <script>window.isoData = ${JSON.stringify(isoData)
-    .split(">")
-    .join("&gt;")}</script>
+  <script>window.isoData = ${sanitize(JSON.stringify(isoData))}</script>
   <script>window.lemmyConfig = ${serialize(config)}</script>
 
   <!-- A remote debugging utility for mobile -->
