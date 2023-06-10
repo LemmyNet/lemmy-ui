4da24386383a4150dc1634869a6ad11ece1f625f
Sean Spade <skspade@pm.me>
2023-06-09 23:28:56 -0400

    chore: ensures validURL function does not throw exception

--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -327,7 +327,12 @@ export function isVideo(url: string) {
 }
 
 export function validURL(str: string) {
-  return !!new URL(str);
+  try {
+    new URL(str);
+    return true;
+  } catch (_) {
+    return false;
+  }
 }
 
 export function validInstanceTLD(str: string) {
