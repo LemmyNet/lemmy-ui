2a4c566435364dd44348c59224726658a065fe45
Jay Sitter <jay@jaysitter.com>
2023-06-19 15:34:11 -0400

    fix: Fix missing margin in search form

--- src/shared/components/search.tsx
+++ src/shared/components/search.tsx
@@ -183,7 +183,7 @@ const Filter = ({
 }) => {
   return (
     <div className="input-group mb-3 col-sm-6">
-      <label className="col-form-label" htmlFor={`${filterType}-filter`}>
+      <label className="col-form-label me-2" htmlFor={`${filterType}-filter`}>
         {capitalizeFirstLetter(i18n.t(filterType))}
       </label>
       <SearchableSelect
