0bb0c138337202f57aaeaab089843ebfad6b397f
Jay Sitter <jay@jaysitter.com>
2023-06-19 17:15:45 -0400

    feat: Reorganize themes Sass and add Litely Compact

--- src/server/utils/build-themes-list.ts
+++ src/server/utils/build-themes-list.ts
@@ -4,7 +4,13 @@ import { readdir } from "fs/promises";
 const extraThemesFolder =
   process.env["LEMMY_UI_EXTRA_THEMES_FOLDER"] || "./extra_themes";
 
-const themes = ["darkly", "darkly-red", "litely", "litely-red"];
+const themes = [
+  "darkly",
+  "darkly-red",
+  "litely",
+  "litely-red",
+  "litely-compact",
+];
 
 export async function buildThemeList(): Promise<string[]> {
   if (existsSync(extraThemesFolder)) {
