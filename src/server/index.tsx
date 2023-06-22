3664b0600b3a196209248ff7c5898a78ee4c0795
SleeplessOne1917 <abias1122@gmail.com>
2023-06-22 01:04:28 +0000

    (Hopefully) fix webmanifest different origin issue (#1457)
    Co-authored-by: Dessalines <dessalines@users.noreply.github.com>

--- src/server/index.tsx
+++ src/server/index.tsx
@@ -25,7 +25,7 @@ if (!process.env["LEMMY_UI_DISABLE_CSP"] && !process.env["LEMMY_UI_DEBUG"]) {
 
 server.get("/robots.txt", RobotsHandler);
 server.get("/service-worker.js", ServiceWorkerHandler);
-server.get("/manifest", ManifestHandler);
+server.get("/manifest.webmanifest", ManifestHandler);
 server.get("/css/themes/:name", ThemeHandler);
 server.get("/css/themelist", ThemesListHandler);
 server.get("/*", CatchAllHandler);
