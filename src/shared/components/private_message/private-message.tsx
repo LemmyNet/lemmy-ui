0cc354b26431202f9b67ed638ba3ec5be1c3cc61
Jay Sitter <jay@jaysitter.com>
2023-06-25 01:35:34 -0400

    fix(a11y): Change divs and links to buttons

--- src/shared/components/private_message/private-message.tsx
+++ src/shared/components/private_message/private-message.tsx
@@ -109,17 +109,17 @@ export class PrivateMessage extends Component<
               </span>
             </li>
             <li className="list-inline-item">
-              <div
-                role="button"
-                className="pointer text-monospace"
+              <button
+                type="button"
+                className="pointer text-monospace btn p-0 d-block"
                 onClick={linkEvent(this, this.handleMessageCollapse)}
               >
                 {this.state.collapsed ? (
-                  <Icon icon="plus-square" classes="icon-inline" />
+                  <Icon icon="plus-square" />
                 ) : (
-                  <Icon icon="minus-square" classes="icon-inline" />
+                  <Icon icon="minus-square" />
                 )}
-              </div>
+              </button>
             </li>
           </ul>
           {this.state.showEdit && (
