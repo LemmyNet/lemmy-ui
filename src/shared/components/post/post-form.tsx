9dc25f2247e77dcc8bc410ec4cb30c27b99e7c7b
Dominic Mazzoni <dmazzoni@gmail.com>
2023-06-25 21:11:23 -0700

    Associate NSFW label with its checkbox

--- src/shared/components/post/post-form.tsx
+++ src/shared/components/post/post-form.tsx
@@ -528,7 +528,7 @@ export class PostForm extends Component<PostFormProps, PostFormState> {
               checked={this.state.form.nsfw}
               onChange={linkEvent(this, handlePostNsfwChange)}
             />
-            <label className="form-check-label">
+            <label className="form-check-label" htmlFor="post-nsfw">
               {I18NextService.i18n.t("nsfw")}
             </label>
           </div>
