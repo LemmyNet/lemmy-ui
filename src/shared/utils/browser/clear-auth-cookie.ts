28e885d599427c42cb6b87f19ed891311150ebd7
Alec Armbruster <35377827+alectrocute@users.noreply.github.com>
2023-07-04 11:56:51 -0400

    remove expires

--- src/shared/utils/browser/clear-auth-cookie.ts
+++ src/shared/utils/browser/clear-auth-cookie.ts
@@ -3,7 +3,6 @@ import { authCookieName } from "../../config";
 
 export default function clearAuthCookie() {
   document.cookie = cookie.serialize(authCookieName, "", {
-    expires: new Date("Thu, 01 Jan 1970 00:00:01 GMT"),
     maxAge: -1,
     sameSite: true,
     path: "/",
