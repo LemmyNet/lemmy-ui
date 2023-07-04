1b162211e10e1b6a30b4b04e966d0486d5bb0ec0
SleeplessOne1917 <abias1122@gmail.com>
2023-07-04 12:02:51 -0400

    Add other dark themes

--- src/shared/utils/browser/data-bs-theme.ts
+++ src/shared/utils/browser/data-bs-theme.ts
@@ -4,9 +4,13 @@ import isDark from "./is-dark";
 export default function dataBsTheme(user?: MyUserInfo) {
   return (isDark() && user?.local_user_view.local_user.theme === "browser") ||
     (user &&
-      ["darkly", "darkly-red", "darkly-pureblack"].includes(
-        user.local_user_view.local_user.theme
-      ))
+      [
+        "darkly",
+        "darkly-red",
+        "darkly-pureblack",
+        "darkly-compact",
+        "i386",
+      ].includes(user.local_user_view.local_user.theme))
     ? "dark"
     : "light";
 }
