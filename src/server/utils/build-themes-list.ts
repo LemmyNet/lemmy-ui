3b501337af755026b82568cc7dd163e1a9f409d7
SleeplessOne1917 <abias1122@gmail.com>
2023-07-04 12:06:31 -0400

    Run prettier

--- src/server/utils/build-themes-list.ts
+++ src/server/utils/build-themes-list.ts
@@ -14,7 +14,7 @@ const themes: ReadonlyArray<string> = [
   "litely-compact",
   "vaporwave-dark",
   "vaporwave-light",
-  "i386"
+  "i386",
 ];
 
 export async function buildThemeList(): Promise<ReadonlyArray<string>> {
