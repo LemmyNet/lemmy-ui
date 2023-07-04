367856bf7628188bdcffd591b3561a69494d2cd6
SleeplessOne1917 <abias1122@gmail.com>
2023-07-04 12:00:34 -0400

    Fix data-bs-theme

--- src/shared/utils/browser/data-bs-theme.ts
+++ src/shared/utils/browser/data-bs-theme.ts
@@ -1,6 +1,7 @@
+import { MyUserInfo } from "lemmy-js-client";
 import isDark from "./is-dark";
 
-export default function dataBsTheme(user) {
+export default function dataBsTheme(user?: MyUserInfo) {
   return (isDark() && user?.local_user_view.local_user.theme === "browser") ||
     (user &&
       ["darkly", "darkly-red", "darkly-pureblack"].includes(
