d24211efb036f1ca999944e11da565726b437f2b
Pavlos Smith <57727226+iByteABit256@users.noreply.github.com>
2023-07-19 18:30:36 +0300

    Removed newline

--- src/shared/components/person/inbox.tsx
+++ src/shared/components/person/inbox.tsx
@@ -214,7 +214,6 @@ export class Inbox extends Component<any, InboxState> {
   render() {
     const auth = myAuth();
     const inboxRss = auth ? `/feeds/inbox/${auth}.xml` : undefined;
-
     return (
       <div className="inbox container-lg">
         <div className="row">
