bd24b87b714ada60541f931c3c85fe2a8065a563
Someonestolemynickname <twit@arcor.de>
2023-07-20 14:03:02 +0200

    Expanded the RegEx to check if the title contains new line caracters. Should fix the issue #1962. Also added Comments for clarity.

--- src/shared/utils/helpers/valid-title.ts
+++ src/shared/utils/helpers/valid-title.ts
@@ -2,7 +2,14 @@ export default function validTitle(title?: string): boolean {
   // Initial title is null, minimum length is taken care of by textarea's minLength={3}
   if (!title || title.length < 3) return true;
 
-  const regex = new RegExp(/.*\S.*/, "g");
+
+  /*
+    Test if the Title is in a valid format:
+      (?=.*\S.*) checks if the title consits of only whitespace characters
+      (?=^[^\r\n]+$) checks if the title contains newlines 
+  */
+  const regex = new RegExp(/(?=(.*\S.*))(?=^[^\r\n]+$)/, "g");
+
 
   return regex.test(title);
 }
