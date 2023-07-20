06ef540a94c90a8f642b31eaaf1edd6f642b1dc1
someonestolemynickname <twit@arcor.de>
2023-07-20 20:16:28 +0200

    ran yarn and changed according to recommendations

--- src/shared/utils/helpers/valid-title.ts
+++ src/shared/utils/helpers/valid-title.ts
@@ -2,14 +2,12 @@ export default function validTitle(title?: string): boolean {
   // Initial title is null, minimum length is taken care of by textarea's minLength={3}
   if (!title || title.length < 3) return true;
 
-
   /*
     Test if the Title is in a valid format:
-      (?=.*\S.*) checks if the title consits of only whitespace characters
+      (?=.*\S.*) checks if the title consists of only whitespace characters
       (?=^[^\r\n]+$) checks if the title contains newlines 
   */
   const regex = new RegExp(/(?=(.*\S.*))(?=^[^\r\n]+$)/, "g");
-
 
   return regex.test(title);
 }
