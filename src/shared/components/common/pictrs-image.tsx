9fd75faa16c975152dfa4c35ab1f4891aba8ae96
Domenic Horner <domenic@tgxn.net>
2023-08-08 01:48:55 +0800

    Frontend Settings - "Blur NSFW" and "Auto Expand" (#1640)
    * fix for en-AU locale
    
    * add setting checkboxes for auto-expand and blur nsfw
    
    * mappings for backend
    
    * blur and nsfw settings
    
    * add icon blur
    
    * fix lint
    
    * revert remove en-au

--- src/shared/components/common/pictrs-image.tsx
+++ src/shared/components/common/pictrs-image.tsx
@@ -1,6 +1,8 @@
 import classNames from "classnames";
 import { Component } from "inferno";
 
+import { UserService } from "../../services";
+
 const iconThumbnailSize = 96;
 const thumbnailSize = 256;
 
@@ -21,6 +23,14 @@ export class PictrsImage extends Component<PictrsImageProps, any> {
   }
 
   render() {
+    let user_blur_nsfw = true;
+    if (UserService.Instance.myUserInfo) {
+      user_blur_nsfw =
+        UserService.Instance.myUserInfo?.local_user_view.local_user.blur_nsfw;
+    }
+
+    const blur_image = this.props.nsfw && user_blur_nsfw;
+
     return (
       <picture>
         <source srcSet={this.src("webp")} type="image/webp" />
@@ -38,7 +48,8 @@ export class PictrsImage extends Component<PictrsImageProps, any> {
               this.props.thumbnail && !this.props.icon && !this.props.banner,
             "img-expanded slight-radius":
               !this.props.thumbnail && !this.props.icon,
-            "img-blur": this.props.thumbnail && this.props.nsfw,
+            "img-blur-icon": this.props.icon && blur_image,
+            "img-blur-thumb": this.props.thumbnail && blur_image,
             "object-fit-cover img-icon me-1": this.props.icon,
             "ms-2 mb-0 rounded-circle object-fit-cover avatar-overlay":
               this.props.iconOverlay,
--- src/shared/components/post/post-listing.tsx
+++ src/shared/components/post/post-listing.tsx
@@ -169,6 +169,11 @@ export class PostListing extends Component<PostListingProps, PostListingState> {
   constructor(props: any, context: any) {
     super(props, context);
 
+    if (UserService.Instance.myUserInfo) {
+      this.state.imageExpanded =
+        UserService.Instance.myUserInfo.local_user_view.local_user.auto_expand;
+    }
+
     this.handleEditPost = this.handleEditPost.bind(this);
     this.handleEditCancel = this.handleEditCancel.bind(this);
   }
@@ -297,6 +302,7 @@ export class PostListing extends Component<PostListingProps, PostListingState> {
 
   imgThumb(src: string) {
     const post_view = this.postView;
+
     return (
       <PictrsImage
         src={src}
