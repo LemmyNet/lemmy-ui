c10647463620433f6d7d4e1bd07e17ad66fd6ca3
SleeplessOne1917 <abias1122@gmail.com>
2023-08-31 12:34:06 +0000

    Fix communities search bug

---
+++ src/shared/components/common/report-form.tsx
@@ -0,0 +1,69 @@
+import { Component, linkEvent } from "inferno";
+import { I18NextService } from "../../services/I18NextService";
+import { Spinner } from "./icon";
+import { randomStr } from "@utils/helpers";
+
+interface ReportFormProps {
+  onSubmit: (reason: string) => void;
+}
+
+interface ReportFormState {
+  loading: boolean;
+  reason: string;
+}
+
+function handleReportReasonChange(i: ReportForm, event: any) {
+  i.setState({ reason: event.target.value });
+}
+
+function handleReportSubmit(i: ReportForm, event: any) {
+  event.preventDefault();
+  i.setState({ loading: true });
+  i.props.onSubmit(i.state.reason);
+
+  i.setState({
+    loading: false,
+    reason: "",
+  });
+}
+
+export default class ReportForm extends Component<
+  ReportFormProps,
+  ReportFormState
+> {
+  state: ReportFormState = {
+    loading: false,
+    reason: "",
+  };
+  constructor(props, context) {
+    super(props, context);
+  }
+
+  render() {
+    const { loading, reason } = this.state;
+    const id = `report-form-${randomStr()}`;
+
+    return (
+      <form
+        className="form-inline"
+        onSubmit={linkEvent(this, handleReportSubmit)}
+      >
+        <label className="visually-hidden" htmlFor={id}>
+          {I18NextService.i18n.t("reason")}
+        </label>
+        <input
+          type="text"
+          id={id}
+          className="form-control me-2"
+          placeholder={I18NextService.i18n.t("reason")}
+          required
+          value={reason}
+          onInput={linkEvent(this, handleReportReasonChange)}
+        />
+        <button type="submit" className="btn btn-secondary">
+          {loading ? <Spinner /> : I18NextService.i18n.t("create_report")}
+        </button>
+      </form>
+    );
+  }
+}
