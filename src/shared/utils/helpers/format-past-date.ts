01db851785997c725a920010d886c6ec19406f23
Dessalines <tyhou13@gmx.com>
2023-09-29 07:43:30 -0400

    Revert "Added error handling to format-past-date to prevent timeouts on modlog (#2083)"
    This reverts commit 02bb1b84e1220cbf25b25eaabf1017083906b277.

--- src/shared/utils/helpers/format-past-date.ts
+++ src/shared/utils/helpers/format-past-date.ts
@@ -2,23 +2,8 @@ import formatDistanceStrict from "date-fns/formatDistanceStrict";
 import parseISO from "date-fns/parseISO";
 
 export default function (dateString?: string) {
-  if (!dateString) {
-    console.error(err);
-    return "DATE ERROR";
-  }
-
-  try {
-    const parsed = parseISO(Date.now().toString() + "Z");
-    return formatDistanceStrict(parsed, new Date(), {
-      addSuffix: true,
-    });
-  } catch (err) {
-    console.error(err);
-    if (err instanceof RangeError) {
-      console.error(
-        `Got the invalid value of ${dateString} when attempting to parse to ISO date`,
-      );
-      return "DATE ERROR";
-    }
-  }
+  const parsed = parseISO((dateString ?? Date.now().toString()) + "Z");
+  return formatDistanceStrict(parsed, new Date(), {
+    addSuffix: true,
+  });
 }
