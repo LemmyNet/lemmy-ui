2ee0d3622f10f50e6a79b7b7bf4176100a8db530
SleeplessOne <insomnia-void@protonmail.com>
2023-12-05 21:56:50 -0500

    Remove forward header

--- src/server/handlers/manifest-handler.ts
+++ src/server/handlers/manifest-handler.ts
@@ -3,17 +3,13 @@ import type { Request, Response } from "express";
 import { LemmyHttp } from "lemmy-js-client";
 import { wrapClient } from "../../shared/services/HttpService";
 import generateManifestJson from "../utils/generate-manifest-json";
-import { setForwardedHeaders } from "../utils/set-forwarded-headers";
 
 let manifest: Awaited<ReturnType<typeof generateManifestJson>> | undefined =
   undefined;
 
-export default async (req: Request, res: Response) => {
+export default async (_req: Request, res: Response) => {
   if (!manifest) {
-    const headers = setForwardedHeaders(req.headers);
-    const client = wrapClient(
-      new LemmyHttp(getHttpBaseInternal(), { headers }),
-    );
+    const client = wrapClient(new LemmyHttp(getHttpBaseInternal()));
     const site = await client.getSite();
 
     if (site.state === "success") {
