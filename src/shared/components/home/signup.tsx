df570c64f070fe70c3ca1c8a74e950028c1ade0e
SleeplessOne1917 <insomnia-void@protonmail.com>
2024-01-14 19:21:59 -0500

    Fix captcha cache control bug

--- src/shared/components/home/signup.tsx
+++ src/shared/components/home/signup.tsx
@@ -71,9 +71,13 @@ export class Signup extends Component<any, State> {
 
   async fetchCaptcha() {
     this.setState({ captchaRes: LOADING_REQUEST });
+    HttpService.client.setHeaders({
+      "Cache-Control": "private, no-cache",
+    });
     this.setState({
       captchaRes: await HttpService.client.getCaptcha(),
     });
+    HttpService.client.setHeaders({});
 
     this.setState(s => {
       if (s.captchaRes.state === "success") {
