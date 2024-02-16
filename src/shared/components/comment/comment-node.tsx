77cd46b35bbb12b42e81261d30c43c1d89751046
Die4Ever <30947252+Die4Ever@users.noreply.github.com>
2024-02-16 09:20:49 -0600

    always show fedilinks (issue #2160) (#2344)
    * always show fedilinks (issue #2160)
    
    * fix comment fedilinks title text
    
    * fix comment links title text
    
    * remove unused local variable
    
    ---------
    
    Co-authored-by: SleeplessOne1917 <28871516+SleeplessOne1917@users.noreply.github.com>

--- src/shared/components/comment/comment-node.tsx
+++ src/shared/components/comment/comment-node.tsx
@@ -507,11 +507,13 @@ export class CommentNode extends Component<CommentNodeProps, CommentNodeState> {
         >
           <Icon icon="link" classes="icon-inline" />
         </Link>
-        {!cv.comment.local && (
-          <a className={classnames} title={title} href={cv.comment.ap_id}>
-            <Icon icon="fedilink" classes="icon-inline" />
-          </a>
-        )}
+        <a
+          className={classnames}
+          title={I18NextService.i18n.t("link")}
+          href={cv.comment.ap_id}
+        >
+          <Icon icon="fedilink" classes="icon-inline" />
+        </a>
       </>
     );
   }
