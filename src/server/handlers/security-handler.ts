1ff4acc0492c52bc1279c79472cd732eb8d384f8
Dessalines <dessalines@users.noreply.github.com>
2024-02-16 10:17:15 -0500

    Changing security.txt to use github security advisories page. (#2334)
    * Changing security.txt to use github security advisories page.
    
    - Fixes #2332
    
    * Adding an expires date, one year from build date.
    
    * Add a year to the build date in code.
    
    * Fix dev.dockerfile build date.
    
    ---------
    
    Co-authored-by: SleeplessOne1917 <28871516+SleeplessOne1917@users.noreply.github.com>

--- src/server/handlers/security-handler.ts
+++ src/server/handlers/security-handler.ts
@@ -1,17 +1,20 @@
 import type { Response } from "express";
+import { BUILD_DATE_ISO8601 } from "../../shared/build-date";
+import { parseISO } from "date-fns";
 
 export default async ({ res }: { res: Response }) => {
+  const buildDatePlusYear = parseISO(BUILD_DATE_ISO8601);
+
+  // Add a year to the build date
+  buildDatePlusYear.setFullYear(new Date().getFullYear() + 1);
+
+  const yearFromNow = buildDatePlusYear.toISOString();
+
   res.setHeader("content-type", "text/plain; charset=utf-8");
 
   res.send(
-    `Contact: mailto:security@lemmy.ml
-  Contact: mailto:admin@` +
-      process.env.LEMMY_UI_LEMMY_EXTERNAL_HOST +
-      `
-  Contact: mailto:security@` +
-      process.env.LEMMY_UI_LEMMY_EXTERNAL_HOST +
-      `
-  Expires: 2024-01-01T04:59:00.000Z
+    `Contact: https://github.com/LemmyNet/lemmy-ui/security/advisories/new
+  Expires: ${yearFromNow}
   `,
   );
 };
