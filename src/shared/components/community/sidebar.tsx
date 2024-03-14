a860bdd70098eb4a41e091403e9cbc363780248e
SleeplessOne1917 <28871516+SleeplessOne1917@users.noreply.github.com>
2024-03-14 16:09:11 +0000

    Community visibility setting (#2376)
    * Update deps
    
    * Add community visibility to community form for editing and creating
    
    * Add translations
    
    * Make visibility display
    
    * Add translations
    
    ---------
    
    Co-authored-by: SleeplessOne1917 <insomnia-void@protonmail.com>
    Co-authored-by: Dessalines <dessalines@users.noreply.github.com>

--- src/shared/components/community/sidebar.tsx
+++ src/shared/components/community/sidebar.tsx
@@ -124,7 +124,7 @@ export class Sidebar extends Component<SidebarProps, SidebarState> {
   sidebar() {
     const myUserInfo = UserService.Instance.myUserInfo;
     const {
-      community: { name, actor_id, id, posting_restricted_to_mods },
+      community: { name, actor_id, id, posting_restricted_to_mods, visibility },
       counts,
     } = this.props.community_view;
     return (
@@ -175,6 +175,30 @@ export class Sidebar extends Component<SidebarProps, SidebarState> {
                 </div>
               )}
               {this.description()}
+              <div>
+                <div className="fw-semibold mb-1">
+                  <span className="align-middle">
+                    {I18NextService.i18n.t("community_visibility")}:&nbsp;
+                  </span>
+                  <span className="fs-5 fw-medium align-middle">
+                    {I18NextService.i18n.t(
+                      visibility === "Public" ? "public" : "local_only",
+                    )}
+                    <Icon
+                      icon={visibility === "Public" ? "globe" : "house"}
+                      inline
+                      classes="ms-1 text-secondary"
+                    />
+                  </span>
+                </div>
+                <p>
+                  {I18NextService.i18n.t(
+                    visibility === "Public"
+                      ? "public_blurb"
+                      : "local_only_blurb",
+                  )}
+                </p>
+              </div>
               <Badges communityId={id} counts={counts} />
               {this.mods()}
             </div>
