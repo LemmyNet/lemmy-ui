f35f27808f8ad56a6ca861f31b6d70bad42175d3
SleeplessOne1917 <28871516+SleeplessOne1917@users.noreply.github.com>
2024-03-26 22:12:07 -0400

    Add matrix account link to create private message form

--- src/shared/components/private_message/private-message-form.tsx
+++ src/shared/components/private_message/private-message-form.tsx
@@ -94,6 +94,20 @@ export class PrivateMessageForm extends Component<
               #
             </a>
           </T>
+          {this.props.recipient.matrix_user_id && (
+            <>
+              &nbsp;This user already has a matrix account you can reach
+              at&nbsp;
+              <a
+                className="alert-link"
+                rel={relTags}
+                href={`https://matrix.to/#/${this.props.recipient.matrix_user_id}`}
+              >
+                {this.props.recipient.matrix_user_id}
+              </a>
+              .
+            </>
+          )}
         </div>
         <div className="mb-3 row">
           <label className="col-sm-2 col-form-label">
@@ -101,7 +115,7 @@ export class PrivateMessageForm extends Component<
           </label>
           <div className="col-sm-10">
             <MarkdownTextArea
-              onSubmit={() => {
+              onSubmit={event => {
                 this.handlePrivateMessageSubmit(this, event);
               }}
               initialContent={this.state.content}
