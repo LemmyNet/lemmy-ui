f9f2f172344eac85dd834d5316ea7d26d22e0076
SleeplessOne1917 <28871516+SleeplessOne1917@users.noreply.github.com>
2024-05-10 15:12:37 -0400

    Fix leap year issue

--- src/shared/utils/helpers/is-cake-day.ts
+++ src/shared/utils/helpers/is-cake-day.ts
@@ -1,11 +1,34 @@
-import { parseISO, getYear, getDayOfYear } from "date-fns";
+import { parseISO, getYear, getDayOfYear, isLeapYear } from "date-fns";
+
+const leapDay = getDayOfYear(new Date(2024, 1, 29));
 
 export default function isCakeDay(published: string): boolean {
   const createDate = parseISO(published);
+  const createDateDayOfYear = getDayOfYear(createDate);
+  const isCreateDateLeapYear = isLeapYear(createDate);
+
   const currentDate = new Date();
+  let currentDateDayOfYear = getDayOfYear(currentDate);
+  const isCurrentDateLeapYear = isLeapYear(currentDate);
+
+  if (
+    isCreateDateLeapYear &&
+    !isCurrentDateLeapYear &&
+    currentDateDayOfYear >= leapDay
+  ) {
+    ++currentDateDayOfYear;
+  }
+
+  if (
+    !isCreateDateLeapYear &&
+    isCurrentDateLeapYear &&
+    createDateDayOfYear >= leapDay
+  ) {
+    --currentDateDayOfYear;
+  }
 
   return (
-    getDayOfYear(createDate) === getDayOfYear(currentDate) &&
+    createDateDayOfYear === currentDateDayOfYear &&
     getYear(createDate) !== getYear(currentDate)
   );
 }
