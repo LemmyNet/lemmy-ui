48b0e0369d9d3e3c24a3d8ccfd033926d821b66e
Matthias Camenzind <matc.pub@protonmail.com>
2024-05-12 08:08:00 +0200

    Remove commented out assertions

--- src/shared/utils/helpers/is-cake-day.ts
+++ src/shared/utils/helpers/is-cake-day.ts
@@ -6,9 +6,9 @@ export function cakeDate(published: string): Date {
   return parse(published.substring(0, 10), "yyyy-MM-dd", new Date(0));
 }
 
-export default function isCakeDay(published: string, current?: Date): boolean {
+export default function isCakeDay(published: string): boolean {
   const createDate = cakeDate(published);
-  const currentDate = current ?? new Date();
+  const currentDate = new Date();
 
   // The day-overflow of Date makes leap days become 03-01 in non leap years.
   return (
@@ -16,63 +16,3 @@ export default function isCakeDay(published: string, current?: Date): boolean {
     !isSameYear(currentDate, createDate)
   );
 }
-
-/*
-import { format } from "date-fns";
-// Note: Systems with UTC+0 can pass these even when they shouldn't.
-console.assert(
-  isCakeDay("2023-05-11T00:00:00.000Z", new Date(2024, 4, 11)),
-  "basic, early",
-);
-console.assert(
-  isCakeDay("2023-05-11T23:59:00.000Z", new Date(2024, 4, 11)),
-  "basic, late",
-);
-
-console.assert(
-  !isCakeDay("2024-05-11T00:00:00.000Z", new Date(2024, 4, 11)),
-  "not today, early",
-);
-console.assert(
-  !isCakeDay("2024-05-11T23:59:00.000Z", new Date(2024, 4, 11)),
-  "not today, late",
-);
-
-console.assert(
-  isCakeDay("2020-02-29T00:00:00.000Z", new Date(2024, 1, 29)),
-  "leap day, leap year",
-);
-console.assert(
-  isCakeDay("2020-02-29T00:00:00.000Z", new Date(2025, 2, 1)),
-  "leap day, non leap year",
-);
-
-console.assert(
-  isCakeDay("2023-03-01T00:00:00.000Z", new Date(2024, 2, 1)),
-  "first of march, leap year",
-);
-console.assert(
-  isCakeDay("2023-03-01T00:00:00.000Z", new Date(2024, 2, 1)),
-  "first of march, non leap year",
-);
-
-console.assert(
-  isCakeDay("2020-03-01T00:00:00.000Z", new Date(2024, 2, 1)),
-  "first of march leap year, leap year",
-);
-console.assert(
-  isCakeDay("2020-03-01T00:00:00.000Z", new Date(2024, 2, 1)),
-  "first of march leap year, non leap year",
-);
-
-// This is how profile.tsx displays the date.
-console.assert(
-  format(cakeDate("2023-05-11T00:00:00.000Z"), "PPP") ===
-    format(cakeDate("2023-05-11T23:59:00.000Z"), "PPP"),
-);
-// This one depends on locales.
-// console.assert(
-//   format(cakeDate("2023-05-11T23:59:00.000Z"), "PPP") === "May 11th, 2023",
-//   format(cakeDate("2023-05-11T23:59:00.000Z"), "PPP"),
-// );
-*/
