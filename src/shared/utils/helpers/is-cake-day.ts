d705f3685fe3ad699b446f5a6167a75d9439a590
SleeplessOne1917 <28871516+SleeplessOne1917@users.noreply.github.com>
2024-05-13 22:45:33 -0400

    Fix leap year issue (#2453)
    * Fix leap year issue
    
    * Show same cake day date independent of timezone (#2455)
    
    * Show same cake day date independent of timezone
    
    * Remove commented out assertions
    
    ---------
    
    Co-authored-by: matc-pub <161147791+matc-pub@users.noreply.github.com>

--- src/shared/utils/helpers/is-cake-day.ts
+++ src/shared/utils/helpers/is-cake-day.ts
@@ -1,11 +1,18 @@
-import { parseISO, getYear, getDayOfYear } from "date-fns";
+import { getYear, isSameDay, isSameYear, parse, setYear } from "date-fns";
+
+// Returns a date in local time with the same year, month and day. Ignores the
+// source timezone. The goal is to show the same date in all timezones.
+export function cakeDate(published: string): Date {
+  return parse(published.substring(0, 10), "yyyy-MM-dd", new Date(0));
+}
 
 export default function isCakeDay(published: string): boolean {
-  const createDate = parseISO(published);
+  const createDate = cakeDate(published);
   const currentDate = new Date();
 
+  // The day-overflow of Date makes leap days become 03-01 in non leap years.
   return (
-    getDayOfYear(createDate) === getDayOfYear(currentDate) &&
-    getYear(createDate) !== getYear(currentDate)
+    isSameDay(currentDate, setYear(createDate, getYear(currentDate))) &&
+    !isSameYear(currentDate, createDate)
   );
 }
