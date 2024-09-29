5d908524c53d991ac3fcab987fc09e2241ca1f0a
Matthias Camenzind <matc.pub@protonmail.com>
2024-09-29 12:49:14 +0200

    Cleanup SortSelect

--- src/shared/components/common/sort-select.tsx
+++ src/shared/components/common/sort-select.tsx
@@ -60,13 +60,13 @@ export class SortSelect extends Component<
         <option disabled aria-hidden="true">
           {I18NextService.i18n.t("sort_type")}
         </option>
-        <option value={"Hot"}>{I18NextService.i18n.t("hot")}</option>
-        <option value={"Top"}>{I18NextService.i18n.t("top")}</option>
-        <option value={"Controversial"}>
+        <option value="Hot">{I18NextService.i18n.t("hot")}</option>
+        <option value="Top">{I18NextService.i18n.t("top")}</option>
+        <option value="Controversial">
           {I18NextService.i18n.t("controversial")}
         </option>
-        <option value={"New"}>{I18NextService.i18n.t("new")}</option>
-        <option value={"Old"}>{I18NextService.i18n.t("old")}</option>
+        <option value="New">{I18NextService.i18n.t("new")}</option>
+        <option value="Old">{I18NextService.i18n.t("old")}</option>
       </select>
     );
   }
@@ -85,53 +85,53 @@ export class SortSelect extends Component<
           {I18NextService.i18n.t("sort_type")}
         </option>
         {!this.props.hideHot && [
-          <option key={"Hot"} value={"Hot"}>
+          <option key="Hot" value="Hot">
             {I18NextService.i18n.t("hot")}
           </option>,
-          <option key={"Active"} value={"Active"}>
+          <option key="Active" value="Active">
             {I18NextService.i18n.t("active")}
           </option>,
-          <option key={"Scaled"} value={"Scaled"}>
+          <option key="Scaled" value="Scaled">
             {I18NextService.i18n.t("scaled")}
           </option>,
         ]}
-        <option value={"Controversial"}>
+        <option value="Controversial">
           {I18NextService.i18n.t("controversial")}
         </option>
-        <option value={"New"}>{I18NextService.i18n.t("new")}</option>
-        <option value={"Old"}>{I18NextService.i18n.t("old")}</option>
+        <option value="New">{I18NextService.i18n.t("new")}</option>
+        <option value="Old">{I18NextService.i18n.t("old")}</option>
         {!this.props.hideMostComments && [
-          <option key={"MostComments"} value={"MostComments"}>
+          <option key="MostComments" value="MostComments">
             {I18NextService.i18n.t("most_comments")}
           </option>,
-          <option key={"NewComments"} value={"NewComments"}>
+          <option key="NewComments" value="NewComments">
             {I18NextService.i18n.t("new_comments")}
           </option>,
         ]}
         <option disabled aria-hidden="true">
           ─────
         </option>
-        <option value={"TopHour"}>{I18NextService.i18n.t("top_hour")}</option>
-        <option value={"TopSixHour"}>
+        <option value="TopHour">{I18NextService.i18n.t("top_hour")}</option>
+        <option value="TopSixHour">
           {I18NextService.i18n.t("top_six_hours")}
         </option>
-        <option value={"TopTwelveHour"}>
+        <option value="TopTwelveHour">
           {I18NextService.i18n.t("top_twelve_hours")}
         </option>
-        <option value={"TopDay"}>{I18NextService.i18n.t("top_day")}</option>
-        <option value={"TopWeek"}>{I18NextService.i18n.t("top_week")}</option>
-        <option value={"TopMonth"}>{I18NextService.i18n.t("top_month")}</option>
-        <option value={"TopThreeMonths"}>
+        <option value="TopDay">{I18NextService.i18n.t("top_day")}</option>
+        <option value="TopWeek">{I18NextService.i18n.t("top_week")}</option>
+        <option value="TopMonth">{I18NextService.i18n.t("top_month")}</option>
+        <option value="TopThreeMonths">
           {I18NextService.i18n.t("top_three_months")}
         </option>
-        <option value={"TopSixMonths"}>
+        <option value="TopSixMonths">
           {I18NextService.i18n.t("top_six_months")}
         </option>
-        <option value={"TopNineMonths"}>
+        <option value="TopNineMonths">
           {I18NextService.i18n.t("top_nine_months")}
         </option>
-        <option value={"TopYear"}>{I18NextService.i18n.t("top_year")}</option>
-        <option value={"TopAll"}>{I18NextService.i18n.t("top_all")}</option>
+        <option value="TopYear">{I18NextService.i18n.t("top_year")}</option>
+        <option value="TopAll">{I18NextService.i18n.t("top_all")}</option>
       </select>
     );
   }
