443ab7fdb77d2b0459dd3bc4aa0f264671201ad7
Dessalines <tyhou13@gmx.com>
2025-02-22 12:22:28 -0500

    Removing pointless media utils

--- src/shared/utils/media/index.ts
+++ src/shared/utils/media/index.ts
@@ -1,4 +1,21 @@
-import isImage from "./is-image";
-import isVideo from "./is-video";
+const imageRegex = /(http)?s?:?(\/\/[^"']*\.(?:jpg|jpeg|gif|png|svg|webp))/;
 
-export { isImage, isVideo };
+export function isImage(url: string) {
+  return imageRegex.test(url);
+}
+
+const videoRegex = /(http)?s?:?(\/\/[^"']*\.(?:mp4|webm))/;
+
+export function isVideo(url: string) {
+  return videoRegex.test(url);
+}
+
+const magnetLinkRegex = /^magnet:\?xt=urn:btih:[0-9a-fA-F]{40,}.*$/;
+
+export function isMagnetLink(url: string) {
+  return magnetLinkRegex.test(url);
+}
+
+export function extractMagnetLinkDownloadName(url: string) {
+  return new URLSearchParams(url).get("dn");
+}
--- src/shared/utils/media/is-video.ts
+++
@@ -1,5 +0,0 @@
-const videoRegex = /(http)?s?:?(\/\/[^"']*\.(?:mp4|webm))/;
-
-export default function isVideo(url: string) {
-  return videoRegex.test(url);
-}
