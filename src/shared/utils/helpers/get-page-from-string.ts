1067937cec00bbfe91042fe051b9f884138c243b
Dessalines <tyhou13@gmx.com>
2025-02-22 12:09:39 -0500

    Removing pointless helpers utils

--- src/shared/utils/helpers/get-page-from-string.ts
+++
@@ -1,3 +0,0 @@
-export default function getPageFromString(page?: string): number {
-  return page && !Number.isNaN(Number(page)) ? Number(page) : 1;
-}
--- src/shared/utils/helpers/valid-instance-tld.ts
+++
@@ -1,5 +0,0 @@
-const tldRegex = /([a-z0-9]+\.)*[a-z0-9]+\.[a-z]+/;
-
-export default function validInstanceTLD(str: string) {
-  return tldRegex.test(str);
-}
