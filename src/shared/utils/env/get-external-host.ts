833967375338cf3e84e0a1a12522e26bd4eab9b1
Solninja A <51935570+SolninjaA@users.noreply.github.com>
2025-03-29 17:52:17 +1000

    First draft of Lemmy on external domains

--- src/shared/utils/env/get-external-host.ts
+++ src/shared/utils/env/get-external-host.ts
@@ -2,13 +2,19 @@ import { isBrowser } from "@utils/browser";
 import { testHost } from "../../config";
 
 export default function getExternalHost() {
-  return isBrowser()
-    ? `${window.location.hostname}${
-        ["1234", "1235"].includes(window.location.port)
-          ? ":8536"
-          : window.location.port === ""
-            ? ""
-            : `:${window.location.port}`
-      }`
-    : process.env.LEMMY_UI_LEMMY_EXTERNAL_HOST || testHost;
+  const externalDomains = process.env.LEMMY_UI_LEMMY_EXTERNAL_DOMAIN === "true";
+
+  if (externalDomains) {
+    return process.env.LEMMY_UI_LEMMY_EXTERNAL_HOST;
+  } else {
+    return isBrowser()
+      ? `${window.location.hostname}${
+          ["1234", "1235"].includes(window.location.port)
+            ? ":8536"
+            : window.location.port === ""
+              ? ""
+              : `:${window.location.port}`
+        }`
+      : process.env.LEMMY_UI_LEMMY_EXTERNAL_HOST || testHost;
+  }
 }
