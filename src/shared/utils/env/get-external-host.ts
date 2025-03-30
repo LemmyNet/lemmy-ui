1e71b7e1808c732a6aff42a4684aeaef7ccd5012
Solninja A <51935570+SolninjaA@users.noreply.github.com>
2025-03-30 11:15:37 +1000

    Changed styling of code

--- src/shared/utils/env/get-external-host.ts
+++ src/shared/utils/env/get-external-host.ts
@@ -2,19 +2,15 @@ import { isBrowser } from "@utils/browser";
 import { testHost } from "../../config";
 
 export default function getExternalHost() {
-  const externalDomains = process.env.LEMMY_UI_LEMMY_EXTERNAL_DOMAIN === "true";
-
-  if (externalDomains) {
-    return process.env.LEMMY_UI_LEMMY_EXTERNAL_HOST;
-  } else {
-    return isBrowser()
-      ? `${window.location.hostname}${
+  return isBrowser()
+    ? process.env.LEMMY_UI_LEMMY_EXTERNAL_DOMAIN === "true"
+      ? process.env.LEMMY_UI_LEMMY_EXTERNAL_HOST
+      : `${window.location.hostname}${
           ["1234", "1235"].includes(window.location.port)
             ? ":8536"
             : window.location.port === ""
               ? ""
               : `:${window.location.port}`
         }`
-      : process.env.LEMMY_UI_LEMMY_EXTERNAL_HOST || testHost;
-  }
+    : process.env.LEMMY_UI_LEMMY_EXTERNAL_HOST || testHost;
 }
