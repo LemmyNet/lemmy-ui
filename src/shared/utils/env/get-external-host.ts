17a86b87633252ca507b83c5e543a17e6145429a
Solninja A <51935570+SolninjaA@users.noreply.github.com>
2025-04-02 16:54:16 +1000

    Remove unnecessary environment variable

--- src/shared/utils/env/get-external-host.ts
+++ src/shared/utils/env/get-external-host.ts
@@ -1,16 +1,5 @@
-import { isBrowser } from "@utils/browser";
 import { testHost } from "../../config";
 
 export default function getExternalHost() {
-  return isBrowser()
-    ? process.env.LEMMY_UI_LEMMY_HAS_EXTERNAL_DOMAIN === "true"
-      ? process.env.LEMMY_UI_LEMMY_EXTERNAL_HOST || testHost
-      : `${window.location.hostname}${
-          ["1234", "1235"].includes(window.location.port)
-            ? ":8536"
-            : window.location.port === ""
-              ? ""
-              : `:${window.location.port}`
-        }`
-    : process.env.LEMMY_UI_LEMMY_EXTERNAL_HOST || testHost;
+  return process.env.LEMMY_UI_LEMMY_EXTERNAL_HOST || testHost;
 }
