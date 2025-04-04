f071aa84953b2121a985b205ea606b09d4195395
Solninja A <51935570+SolninjaA@users.noreply.github.com>
2025-04-04 15:02:13 +1000

    Replace '||' with '??'

--- src/shared/utils/env/get-external-host.ts
+++ src/shared/utils/env/get-external-host.ts
@@ -1,5 +1,5 @@
 import { testHost } from "../../config";
 
 export default function getExternalHost() {
-  return process.env.LEMMY_UI_LEMMY_EXTERNAL_HOST || testHost;
+  return process.env.LEMMY_UI_LEMMY_EXTERNAL_HOST ?? testHost;
 }
