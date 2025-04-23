13f09449e4f843e4b2670e3b9c2976e3081fc7af
MrKaplan <169855803+MrKaplan-lw@users.noreply.github.com>
2025-04-23 19:19:24 +0200

    Remove LEMMY_UI_LEMMY_EXTERNAL_HOST from webpack.DefinePlugin (#3101)
    webpack.DefinePlugin overrides expressions at compile time, but this env var must be evaluated at runtime

--- src/shared/utils/env/get-external-host.ts
+++ src/shared/utils/env/get-external-host.ts
@@ -1,5 +1,8 @@
 import { testHost } from "../../config";
+import { isBrowser } from "@utils/browser";
 
 export default function getExternalHost() {
-  return process.env.LEMMY_UI_LEMMY_EXTERNAL_HOST ?? testHost;
+  return isBrowser()
+    ? window.isoData.lemmy_external_host
+    : (process.env.LEMMY_UI_LEMMY_EXTERNAL_HOST ?? testHost);
 }
