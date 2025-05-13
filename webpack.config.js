2c81273c10186c1c4d970f1dcfbe00b3d969516b
SleeplessOne1917 <28871516+SleeplessOne1917@users.noreply.github.com>
2025-05-13 17:07:35 +0000

    Remove instance cache (#3105)
    * Add max age to instance cache
    
    * Update webpack.config.js
    
    Co-authored-by: MrKaplan <169855803+MrKaplan-lw@users.noreply.github.com>
    
    * Remove instance-cache entirely
    
    * Clear instance cache on logout
    
    ---------
    
    Co-authored-by: MrKaplan <169855803+MrKaplan-lw@users.noreply.github.com>

--- webpack.config.js
+++ webpack.config.js
@@ -132,22 +132,6 @@ module.exports = (env, argv) => {
           inlineWorkboxRuntime: true,
           runtimeCaching: [
             {
-              urlPattern: ({
-                sameOrigin,
-                url: { pathname, host },
-                request: { method },
-              }) =>
-                (sameOrigin || host.includes("localhost")) &&
-                (!(
-                  pathname.includes("pictrs") || pathname.includes("static")
-                ) ||
-                  method === "POST"),
-              handler: "NetworkFirst",
-              options: {
-                cacheName: "instance-cache",
-              },
-            },
-            {
               urlPattern: ({ url: { pathname, host }, sameOrigin }) =>
                 (sameOrigin || host.includes("localhost")) &&
                 pathname.includes("static"),
