869c67053aa92186db72796930fadf24e7cd1606
Richard Schwab <gitrichardschwab-7a2qxq42kj@central-intelligence.agency>
2025-05-29 23:51:49 +0200

    Always escape HTML attributes in emoji autocomplete and custom emoji markdown renderer

--- src/shared/markdown.ts
+++ src/shared/markdown.ts
@@ -209,29 +209,17 @@ export function setupMarkdown() {
   ) {
     //Provide custom renderer for our emojis to allow us to add a css class and force size dimensions on them.
     const item = tokens[idx] as any;
-    const url = item.attrs.length > 0 ? item.attrs[0][1] : "";
-    const altText = item.attrs.length > 1 ? item.attrs[1][1] : "";
     const title = item.attrs.length > 2 ? item.attrs[2][1] : "";
     const splitTitle = title.split(/ (.*)/, 2);
     const isEmoji = splitTitle[0] === "emoji";
-    let shortcode: string | undefined;
-    if (isEmoji) {
-      shortcode = splitTitle[1];
+    const imgElement =
+      defaultImageRenderer?.(tokens, idx, options, env, self) ?? "";
+    if (imgElement) {
+      return isEmoji
+        ? `<span class="icon icon-emoji">${imgElement}</span>`
+        : imgElement;
     }
-    // customEmojisLookup is empty in SSR, CSR rerenders markdown anyway
-    const isLocalEmoji = shortcode && customEmojisLookup.has(shortcode);
-    if (!isLocalEmoji) {
-      const imgElement =
-        defaultImageRenderer?.(tokens, idx, options, env, self) ?? "";
-      if (imgElement) {
-        return `<span class='${
-          isEmoji ? "icon icon-emoji" : ""
-        }'>${imgElement}</span>`;
-      } else return "";
-    }
-    return `<img class="icon icon-emoji" src="${
-      url
-    }" title="${shortcode}" alt="${altText}"/>`;
+    return "";
   };
   md.renderer.rules.table_open = function () {
     return '<table class="table">';
@@ -339,7 +327,7 @@ export async function setupTribute() {
           .concat(
             Array.from(customEmojisLookup.entries()).map(k => ({
               key: k[0],
-              val: `<img class="icon icon-emoji" src="${k[1].custom_emoji.image_url}" title="${k[1].custom_emoji.shortcode}" alt="${k[1].custom_emoji.alt_text}" />`,
+              val: `<img class="icon icon-emoji" src="${md.utils.escapeHtml(k[1].custom_emoji.image_url)}" title="${md.utils.escapeHtml(k[1].custom_emoji.shortcode)}" alt="${md.utils.escapeHtml(k[1].custom_emoji.alt_text)}" />`,
             })),
           ),
         allowSpaces: false,
