6b03ee7e09985ec5579d187fa78b4df9e5fd1f15
Nutomic <me@nutomic.com>
2025-07-01 15:55:54 +0000

    Change display format for community name in search (#3218)

--- src/shared/utils/app/community-select-name.ts
+++ src/shared/utils/app/community-select-name.ts
@@ -4,5 +4,5 @@ import { CommunityView } from "lemmy-js-client";
 export default function communitySelectName(cv: CommunityView): string {
   return cv.community.local
     ? cv.community.title
-    : `${hostname(cv.community.actor_id)}/${cv.community.title}`;
+    : `!${cv.community.title}@${hostname(cv.community.actor_id)}`;
 }
