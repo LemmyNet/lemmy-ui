a3a7d90c2b8e507f5e683c7d34aac374e4dabe6f
Dessalines <dessalines@users.noreply.github.com>
2025-07-31 11:55:09 -0400

    [Main] remove all caches (#3251)
    * Remove all caches (fixes #3195)
    
    * Cherry-picking remove-all-caches
    
    - #3248
    
    * Fixing up.
    
    * Remove cache removals, as these will already be removed previously.
    
    ---------
    
    Co-authored-by: Felix Ableitner <me@nutomic.com>

--- src/shared/services/UserService.ts
+++ src/shared/services/UserService.ts
@@ -55,9 +55,6 @@ export class UserService {
 
     HttpService.client.logout();
 
-    // TODO: Remove this in a few releases when this cache has been deleted from most users' browsers
-    window.caches.delete("instance-cache");
-
     if (isAuthPath(location.pathname)) {
       location.replace("/");
     } else {
