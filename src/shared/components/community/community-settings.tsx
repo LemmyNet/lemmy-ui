b2506d4f57d4aab0e4c586b53387e8c24438c3d1
Dessalines <tyhou13@gmx.com>
2026-01-22 13:31:03 -0500

    Adding ability to create, edit, and delete community tags.

--- src/shared/components/community/community-settings.tsx
+++ src/shared/components/community/community-settings.tsx
@@ -8,7 +8,12 @@ import {
   getApubName,
 } from "@utils/helpers";
 import { scrollMixin } from "../mixins/scroll-mixin";
-import { Choice, RouteDataResponse } from "@utils/types";
+import {
+  Choice,
+  ItemIdAndRes,
+  itemLoading,
+  RouteDataResponse,
+} from "@utils/types";
 import { Component, InfernoNode } from "inferno";
 import { RouteComponentProps } from "inferno-router/dist/Route";
 import {
@@ -16,14 +21,19 @@ import {
   AddModToCommunityResponse,
   CommunityModeratorView,
   CommunityResponse,
+  CreateCommunityTag,
   DeleteCommunity,
+  DeleteCommunityTag,
   EditCommunity,
   GetCommunity,
   GetCommunityResponse,
   LemmyHttp,
   MyUserInfo,
   SuccessResponse,
+  Tag,
+  TagId,
   TransferCommunity,
+  UpdateCommunityTag,
 } from "lemmy-js-client";
 import { InitialFetchRequest } from "@utils/types";
 import { FirstLoadService, I18NextService } from "../../services";
@@ -51,6 +61,8 @@ import { UserBadges } from "@components/common/user-badges";
 import { Icon, Spinner } from "@components/common/icon";
 import { amTopMod, amHigherModerator, amTopModExcludeMe } from "@utils/roles";
 import { SearchableSelect } from "@components/common/searchable-select";
+import { CommunityTagForm } from "./community-tag-form";
+import { NoOptionI18nKeys } from "i18next";
 
 type CommunitySettingsData = RouteDataResponse<{
   communityRes: GetCommunityResponse;
@@ -64,6 +76,8 @@ interface State {
   removeCommunityRes: RequestState<CommunityResponse>;
   leaveModTeamRes: RequestState<AddModToCommunityResponse>;
   purgeCommunityRes: RequestState<SuccessResponse>;
+  createOrUpdateTagRes: ItemIdAndRes<TagId, Tag>;
+  deleteTagRes: ItemIdAndRes<TagId, Tag>;
   isIsomorphic: boolean;
   showLeaveModTeamDialog: boolean;
   // You need to filter by the specific mod id, since this is a jsx loop
@@ -99,6 +113,8 @@ export class CommunitySettings extends Component<RouteProps, State> {
     removeCommunityRes: EMPTY_REQUEST,
     leaveModTeamRes: EMPTY_REQUEST,
     purgeCommunityRes: EMPTY_REQUEST,
+    createOrUpdateTagRes: { id: 0, res: EMPTY_REQUEST },
+    deleteTagRes: { id: 0, res: EMPTY_REQUEST },
     showLeaveModTeamDialog: false,
     showRemoveModDialog: undefined,
     showTransferDialog: undefined,
@@ -190,28 +206,28 @@ export class CommunitySettings extends Component<RouteProps, State> {
       this.state.communityRes.data;
 
     return (
-      getCommunityRes && (
-        <div className="community-settings container">
-          <HtmlTags
-            title={this.documentTitle}
-            path={this.context.router.route.match.url}
-          />
-          <Tabs
-            tabs={[
-              {
-                key: "community",
-                label: I18NextService.i18n.t("community"),
-                getNode: isSelected => (
-                  <div
-                    className={classNames("tab-pane show", {
-                      active: isSelected,
-                    })}
-                    role="tabpanel"
-                    id="community-tab-pane"
-                  >
-                    <h1 className="row justify-content-md-center h4 mb-4">
-                      {I18NextService.i18n.t("settings")}
-                    </h1>
+      <div className="community-settings container">
+        <HtmlTags
+          title={this.documentTitle}
+          path={this.context.router.route.match.url}
+        />
+        <Tabs
+          tabs={[
+            {
+              key: "community",
+              label: I18NextService.i18n.t("community"),
+              getNode: isSelected => (
+                <div
+                  className={classNames("tab-pane show", {
+                    active: isSelected,
+                  })}
+                  role="tabpanel"
+                  id="community-tab-pane"
+                >
+                  <h1 className="row justify-content-md-center h4 mb-4">
+                    {I18NextService.i18n.t("settings")}
+                  </h1>
+                  {getCommunityRes && (
                     <div className="row justify-content-md-center">
                       <div className="col-12 col-md-6">
                         <CommunityForm
@@ -236,48 +252,83 @@ export class CommunitySettings extends Component<RouteProps, State> {
                         />
                       </div>
                     </div>
-                  </div>
-                ),
-              },
-              {
-                key: "moderators",
-                label: I18NextService.i18n.t("mods"),
-                getNode: isSelected => (
-                  <div
-                    className={classNames("tab-pane", {
-                      active: isSelected,
-                    })}
-                    role="tabpanel"
-                    id="users-tab-pane"
-                  >
-                    {this.moderatorsTab()}
-                  </div>
-                ),
-              },
-              {
-                key: "tags",
-                label: I18NextService.i18n.t("tags"),
-                getNode: isSelected => (
-                  <div
-                    className={classNames("tab-pane", {
-                      active: isSelected,
-                    })}
-                    role="tabpanel"
-                    id="taglines-tab-pane"
-                  >
-                    {this.tagsTab()}
-                  </div>
-                ),
-              },
-            ]}
-          />
-        </div>
-      )
+                  )}
+                </div>
+              ),
+            },
+            {
+              key: "moderators",
+              label: I18NextService.i18n.t("mods"),
+              getNode: isSelected => (
+                <div
+                  className={classNames("tab-pane", {
+                    active: isSelected,
+                  })}
+                  role="tabpanel"
+                  id="users-tab-pane"
+                >
+                  {this.moderatorsTab()}
+                </div>
+              ),
+            },
+            {
+              key: "tags",
+              label: I18NextService.i18n.t("tags"),
+              getNode: isSelected => (
+                <div
+                  className={classNames("tab-pane", {
+                    active: isSelected,
+                  })}
+                  role="tabpanel"
+                  id="taglines-tab-pane"
+                >
+                  {this.tagsTab()}
+                </div>
+              ),
+            },
+          ]}
+        />
+      </div>
     );
   }
 
   tagsTab() {
-    // TODO
+    const tags =
+      (this.state.communityRes.state === "success" &&
+        this.state.communityRes.data.community_view.post_tags) ||
+      [];
+    const communityId =
+      (this.state.communityRes.state === "success" &&
+        this.state.communityRes.data.community_view.community.id) ||
+      undefined;
+
+    return (
+      <>
+        <h1 className="h4 mb-4">{I18NextService.i18n.t("tags")}</h1>
+        {tags.map(t => (
+          <CommunityTagForm
+            key={`community-tag-form-${t.id}`}
+            tag={t}
+            onUpdate={form => handleUpdateTag(this, form)}
+            onDelete={form => handleDeleteTag(this, form)}
+            createOrUpdateLoading={
+              itemLoading(this.state.createOrUpdateTagRes) === t.id
+            }
+            deleteLoading={itemLoading(this.state.deleteTagRes) === t.id}
+            myUserInfo={this.isoData.myUserInfo}
+          />
+        ))}
+        {/** The create or empty tag form **/}
+        <CommunityTagForm
+          onCreate={form => handleCreateTag(this, form)}
+          communityId={communityId}
+          createOrUpdateLoading={
+            itemLoading(this.state.createOrUpdateTagRes) === 0
+          }
+          myUserInfo={this.isoData.myUserInfo}
+        />
+      </>
+    );
   }
 
   moderatorsTab() {
@@ -575,6 +626,55 @@ async function handleTransferCommunity(
   if (i.state.transferCommunityRes.state === "success") {
     toast(I18NextService.i18n.t("transfer_community"));
     i.updateCommunityFull(i.state.transferCommunityRes);
+  }
+}
+
+async function handleCreateTag(i: CommunitySettings, form: CreateCommunityTag) {
+  i.setState({ createOrUpdateTagRes: { id: 0, res: LOADING_REQUEST } });
+  const res = await HttpService.client.createCommunityTag(form);
+  i.setState({
+    createOrUpdateTagRes: {
+      id: 0,
+      res,
+    },
+  });
+
+  if (res.state === "success") {
+    toast(I18NextService.i18n.t("community_tag_created"));
+    // Need to refetch community to update tags
+    await i.fetchCommunity(i.props);
+  } else if (res.state === "failed") {
+    toast(I18NextService.i18n.t(res.err.name as NoOptionI18nKeys), "danger");
+  }
+}
+
+async function handleUpdateTag(i: CommunitySettings, form: UpdateCommunityTag) {
+  i.setState({
+    createOrUpdateTagRes: { id: form.tag_id, res: LOADING_REQUEST },
+  });
+  const res = await HttpService.client.updateCommunityTag(form);
+  i.setState({
+    createOrUpdateTagRes: { id: form.tag_id, res },
+  });
+
+  if (res.state === "success") {
+    toast(I18NextService.i18n.t("community_tag_updated"));
+    // Need to refetch community to update tags
+    await i.fetchCommunity(i.props);
+  }
+}
+
+async function handleDeleteTag(i: CommunitySettings, form: DeleteCommunityTag) {
+  i.setState({ deleteTagRes: { id: form.tag_id, res: LOADING_REQUEST } });
+  const res = await HttpService.client.deleteCommunityTag(form);
+  i.setState({
+    deleteTagRes: { id: form.tag_id, res },
+  });
+
+  if (res.state === "success") {
+    toast(I18NextService.i18n.t("community_tag_deleted"));
+    // Need to refetch community to update tags
+    await i.fetchCommunity(i.props);
   }
 }
 
---
+++ src/shared/components/community/community-tag-form.tsx
@@ -0,0 +1,252 @@
+import { randomStr } from "@utils/helpers";
+import { Component, FormEvent, InfernoMouseEvent } from "inferno";
+import {
+  CommunityId,
+  CreateCommunityTag,
+  DeleteCommunityTag,
+  MyUserInfo,
+  Tag,
+  TagId,
+  UpdateCommunityTag,
+} from "lemmy-js-client";
+import { I18NextService } from "../../services";
+import { tippyMixin } from "../mixins/tippy-mixin";
+import { Prompt } from "inferno-router";
+import { Spinner } from "@components/common/icon";
+import { MarkdownTextArea } from "@components/common/markdown-textarea";
+import { validActorRegexPattern } from "@utils/config";
+
+type CommunityTagGenericForm = {
+  tag_id?: TagId;
+  community_id?: CommunityId;
+  name?: string;
+  display_name?: string;
+  description?: string;
+};
+
+interface CommunityTagFormProps {
+  tag?: Tag; // If an tag is given, this means its an edit.
+  communityId?: CommunityId; // Required if its a create.
+  createOrUpdateLoading: boolean;
+  deleteLoading?: boolean;
+  myUserInfo: MyUserInfo | undefined;
+  onCreate?(form: CreateCommunityTag): void;
+  onUpdate?(form: UpdateCommunityTag): void;
+  onDelete?(form: DeleteCommunityTag): void;
+}
+
+interface CommunityTagFormState {
+  form: CommunityTagGenericForm;
+  bypassNavWarning: boolean;
+  // Necessary since markdown doesn't clear after a create submit
+  clearMarkdown: boolean;
+}
+
+function isUpdateForm(
+  form: CommunityTagGenericForm,
+): form is UpdateCommunityTag {
+  return form.tag_id !== undefined;
+}
+
+function isCreateForm(
+  form: CommunityTagGenericForm,
+): form is CreateCommunityTag {
+  return form.tag_id === undefined;
+}
+
+@tippyMixin
+export class CommunityTagForm extends Component<
+  CommunityTagFormProps,
+  CommunityTagFormState
+> {
+  state: CommunityTagFormState = {
+    form: this.createFormFromProps(),
+    bypassNavWarning: true,
+    clearMarkdown: false,
+  };
+
+  createFormFromProps(): CommunityTagGenericForm {
+    const tag = this.props.tag;
+    if (tag) {
+      return {
+        tag_id: tag.id,
+        name: tag.name,
+        display_name: tag.display_name,
+        description: tag.description,
+      };
+    } else {
+      return { community_id: this.props.communityId };
+    }
+  }
+
+  constructor(props: any, context: any) {
+    super(props, context);
+  }
+
+  render() {
+    const submitTitle = I18NextService.i18n.t(
+      this.props.tag ? "save" : "create",
+    );
+
+    const id = randomStr();
+
+    return (
+      <div className="tag-form col-12">
+        <Prompt
+          message={I18NextService.i18n.t("block_leaving")}
+          when={
+            !!(
+              this.state.form.name ||
+              this.state.form.display_name ||
+              this.state.form.description
+            ) && !this.state.bypassNavWarning
+          }
+        />
+        <form
+          className="row row-cols-md-auto g-3 mb-3 align-items-top"
+          onSubmit={e => handleSubmit(this, e)}
+        >
+          {/** Only show the name input if it hasn't been created **/}
+          <div className="col-12">
+            {!this.props.tag ? (
+              <>
+                <label className="visually-hidden" htmlFor={`name-${id}`}>
+                  {I18NextService.i18n.t("column_shortcode")}
+                </label>
+                <input
+                  id={`name--${id}`}
+                  type="text"
+                  placeholder={I18NextService.i18n.t("name")}
+                  className="form-control"
+                  value={this.state.form.name}
+                  required
+                  minLength={3}
+                  pattern={validActorRegexPattern}
+                  onInput={e => handleNameChange(this, e)}
+                />
+              </>
+            ) : (
+              <div>{this.props.tag.name}</div>
+            )}
+          </div>
+          <div className="col-12">
+            <label className="visually-hidden" htmlFor={`display-name-${id}`}>
+              {I18NextService.i18n.t("column_category")}
+            </label>{" "}
+            <input
+              id={`display-name-${id}`}
+              type="text"
+              placeholder={I18NextService.i18n.t("display_name")}
+              className="form-control"
+              value={this.state.form.display_name}
+              pattern="^(?!@)(.+)$"
+              minLength={3}
+              onInput={e => handleDisplayNameChange(this, e)}
+            />
+          </div>
+          <div className="col-12">
+            {!this.state.clearMarkdown && (
+              <MarkdownTextArea
+                initialContent={this.state.form.description}
+                placeholder={I18NextService.i18n.t("description") ?? undefined}
+                onContentChange={s => handleDescriptionChange(this, s)}
+                hideNavigationWarnings
+                allLanguages={[]}
+                siteLanguages={[]}
+                myUserInfo={this.props.myUserInfo}
+              />
+            )}
+          </div>
+          <div className="col-12">
+            <button
+              className="btn btn-secondary me-2"
+              type="submit"
+              disabled={!this.enableForm}
+            >
+              {this.props.createOrUpdateLoading ? <Spinner /> : submitTitle}
+            </button>
+            {this.props.tag && (
+              <button
+                className="btn btn-danger"
+                onClick={e => handleDelete(this, e)}
+              >
+                {this.props.deleteLoading ? (
+                  <Spinner />
+                ) : (
+                  I18NextService.i18n.t("delete")
+                )}
+              </button>
+            )}
+          </div>
+        </form>
+      </div>
+    );
+  }
+
+  get enableForm() {
+    return (this.state.form.name?.length ?? 0) > 0;
+  }
+}
+
+function handleNameChange(
+  i: CommunityTagForm,
+  event: FormEvent<HTMLInputElement>,
+) {
+  i.setState({
+    form: { ...i.state.form, name: event.target.value },
+    bypassNavWarning: false,
+  });
+}
+
+function handleDisplayNameChange(
+  i: CommunityTagForm,
+  event: FormEvent<HTMLInputElement>,
+) {
+  i.setState({
+    form: { ...i.state.form, display_name: event.target.value },
+    bypassNavWarning: false,
+  });
+}
+
+function handleDescriptionChange(i: CommunityTagForm, description: string) {
+  i.setState({
+    form: { ...i.state.form, description },
+    bypassNavWarning: false,
+  });
+}
+
+function handleDelete(
+  i: CommunityTagForm,
+  event: InfernoMouseEvent<HTMLButtonElement>,
+) {
+  event.preventDefault();
+  const tag_id = i.props.tag?.id;
+  if (tag_id) {
+    i.setState({ bypassNavWarning: true });
+    i.props.onDelete?.({ tag_id });
+  }
+}
+
+function handleSubmit(i: CommunityTagForm, event: FormEvent<HTMLFormElement>) {
+  event.preventDefault();
+
+  i.setState({ bypassNavWarning: true });
+
+  const form = i.state.form;
+  if (isUpdateForm(form)) {
+    i.props.onUpdate?.(form);
+  } else if (isCreateForm(form)) {
+    // Clear the markdown
+    i.setState({ clearMarkdown: true });
+    i.setState({
+      clearMarkdown: false,
+      form: {
+        ...i.state.form,
+        description: undefined,
+        name: undefined,
+        display_name: undefined,
+      },
+    });
+    i.props.onCreate?.(form);
+  }
+}
