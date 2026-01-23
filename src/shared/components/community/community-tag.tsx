2a673e4b4d27336e21125436e2652edd6e7d69fc
Dessalines <tyhou13@gmx.com>
2026-01-22 20:23:13 -0500

    Use tag markdown description

--- src/shared/components/community/community-tag.tsx
+++ src/shared/components/community/community-tag.tsx
@@ -1,4 +1,4 @@
-import { RoleLabelPill } from "@components/common/user-badges";
+import { md } from "@utils/markdown";
 import { Tag } from "lemmy-js-client";
 
 type CommunityTagProps = {
@@ -7,9 +7,19 @@ type CommunityTagProps = {
 };
 export function CommunityTag({ tag, useName }: CommunityTagProps) {
   const label = useName ? tag.name : communityTagName(tag);
-  const tooltip = tag.name;
+  const description = tag.description && md.renderInline(tag.description);
+  const tooltip = `${tag.name}${description && ` - ${description}`}`;
 
-  return <RoleLabelPill label={label} tooltip={tooltip} />;
+  return (
+    <span
+      className={`badge text-bg-light"`}
+      aria-label={tooltip}
+      data-tippy-content={tooltip}
+      data-tippy-allowHTML
+    >
+      {label}
+    </span>
+  );
 }
 
 export function communityTagName(tag: Tag): string {
