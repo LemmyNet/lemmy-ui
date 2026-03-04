a249b1d0c05ba8a38a4c201987d3e1ec98c45a20
Felix Ableitner <me@nutomic.com>
2026-03-04 12:02:48 +0100

    another fix

--- src/shared/services/I18NextService.ts
+++ src/shared/services/I18NextService.ts
@@ -3,7 +3,7 @@ import { ImportReport } from "@utils/dynamic-imports";
 import { en } from "../translations/en";
 import { Locale, setDefaultOptions } from "date-fns";
 import { isBrowser } from "@utils/browser";
-import { sync, toast } from "@utils/app";
+import { toast } from "@utils/app";
 
 export type TranslationDesc = {
   // Name of the translation file in `lemmy-translations`
@@ -232,9 +232,7 @@ export async function loadLanguageInstances(
         i18n.addResourceBundle(t.code, "translation", data["translation"]);
       }),
   );
-  await new Promise(r =>
-    sync(i18n.changeLanguage(translationDescs[0].code, r)),
-  );
+  await i18n.changeLanguage(translationDescs[0].code);
 
   return [await localePromise, i18n];
 }
@@ -256,9 +254,7 @@ export async function updateLanguageInstances(
         i18n.addResourceBundle(t.code, "translation", data["translation"]);
       }),
   );
-  await new Promise(r =>
-    sync(i18n.changeLanguage(translationDescs[0].code, r)),
-  );
+  await i18n.changeLanguage(translationDescs[0].code);
   setDefaultOptions({ locale: await locale });
 }
 
