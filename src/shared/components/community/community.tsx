c027c3b3048761154a0689f78fb0a2e8e45302e9
Nutomic <me@nutomic.com>
2026-03-13 16:19:23 +0100

    [0.19] Fix Arabic user/community names (#3968)

--- src/shared/components/community/community.tsx
+++ src/shared/components/community/community.tsx
@@ -274,8 +274,9 @@ export class Community extends Component<CommunityRouteProps, State> {
   async fetchCommunity(props: CommunityRouteProps) {
     const token = (this.fetchCommunityToken = Symbol());
     this.setState({ communityRes: LOADING_REQUEST });
+    const name = decodeURIComponent(props.match.params.name);
     const communityRes = await HttpService.client.getCommunity({
-      name: props.match.params.name,
+      name,
     });
     if (token === this.fetchCommunityToken) {
       this.setState({ communityRes });
@@ -307,7 +308,7 @@ export class Community extends Component<CommunityRouteProps, State> {
     headers,
     query: { dataType, pageCursor, sort, showHidden },
     match: {
-      params: { name: communityName },
+      params: { name: rawCommunityName },
     },
   }: InitialFetchRequest<
     CommunityPathProps,
@@ -317,6 +318,7 @@ export class Community extends Component<CommunityRouteProps, State> {
       new LemmyHttp(getHttpBaseInternal(), { headers }),
     );
 
+    const communityName = decodeURIComponent(rawCommunityName);
     const communityForm: GetCommunity = {
       name: communityName,
     };
@@ -704,7 +706,7 @@ export class Community extends Component<CommunityRouteProps, State> {
   async fetchData(props: CommunityRouteProps) {
     const token = (this.fetchDataToken = Symbol());
     const { dataType, pageCursor, sort, showHidden } = props;
-    const { name } = props.match.params;
+    const name = decodeURIComponent(props.match.params.name);
 
     if (dataType === DataType.Post) {
       this.setState({ postsRes: LOADING_REQUEST, commentsRes: EMPTY_REQUEST });
