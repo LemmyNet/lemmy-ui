a4b5b3e0c15fa0f6d8248c1029c96103f09af2ca
Nutomic <me@nutomic.com>
2026-08-04 15:40:34 +0200

    [0.19] Upgrade dependencies (#4288)
    * [0.19] Upgrade dependencies
    
    * prettier

--- src/shared/components/post/post-form.tsx
+++ src/shared/components/post/post-form.tsx
@@ -156,7 +156,8 @@ function copySuggestedTitle({
   if (suggestedTitle) {
     i.setState(
       s => (
-        (s.form.name = suggestedTitle?.substring(0, MAX_POST_TITLE_LENGTH)), s
+        (s.form.name = suggestedTitle?.substring(0, MAX_POST_TITLE_LENGTH)),
+        s
       ),
     );
     i.setState({ suggestedPostsRes: EMPTY_REQUEST });
