392c974680f5d040894b17ea8f8b4e59367299c9
Felix Ableitner <me@nutomic.com>
2026-08-05 11:26:36 +0900

    restore files

---
+++ src/server/handlers/theme-handler.ts
@@ -0,0 +1,40 @@
+import type { Request, Response } from "express";
+import { existsSync } from "fs";
+import path from "path";
+import { serveCssMaps } from "../utils/dev-env";
+
+const extraThemesFolder =
+  process.env["LEMMY_UI_EXTRA_THEMES_FOLDER"] || "./extra_themes";
+
+export default (req: Request, res: Response) => {
+  const theme = req.params.name;
+
+  if (theme.endsWith(".css")) {
+    res.contentType("text/css");
+  } else if (theme.endsWith(".css.map")) {
+    res.contentType("application/json");
+  }
+
+  if (
+    !theme.endsWith(".css") &&
+    !(serveCssMaps && theme.endsWith(".css.map"))
+  ) {
+    res.status(400).send("Theme must be a css file");
+    return;
+  }
+
+  const customTheme = path.resolve(extraThemesFolder, theme);
+
+  if (existsSync(customTheme)) {
+    res.sendFile(customTheme);
+  } else {
+    const internalTheme = path.resolve(`./dist/assets/css/themes/${theme}`);
+
+    // If the theme doesn't exist, just send litely
+    if (existsSync(internalTheme)) {
+      res.sendFile(internalTheme);
+    } else {
+      res.sendFile(path.resolve("./dist/assets/css/themes/litely.css"));
+    }
+  }
+};
